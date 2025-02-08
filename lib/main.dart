8b84ff603e9d30fcd39e09f5757afe40a988f699
singhpiyush2604 <singhpiyush2604.com>
2025-02-09 00:36:31 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class test extends StatelessWidget {
-  const test({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
