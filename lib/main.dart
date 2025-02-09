998ac63fd3ea4ca676746a003b3d872b76a34adf
singhpiyush2604 <singhpiyush2604.com>
2025-02-09 12:48:02 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:kapde/firebase_options.dart';
 import 'package:kapde/screens/HomeScreen.dart';
 import 'package:kapde/theme/app_theme.dart';
- 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(MyApp());
+// }
+
+void main() {
   runApp(MyApp());
 }
 
@@ -20,7 +24,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme().theme,
-      home: HomeScreen(),
+      home: Test(),
+    );
+  }
+}
+
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(child: Text("hi this is working good work piyush"))
+        ],
+      ),
     );
   }
 }
